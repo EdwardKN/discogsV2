b951802ab8a394530fa8b7e513b04e5e094dfdc5
Edward <edward.klinghammer@gmail.com>
2023-01-16 18:25:50 +0100

    tillbakaknapp

--- script2.js
+++ script2.js
@@ -1,6 +1,10 @@
 document.getElementById("username").value = localStorage.getItem("username");
 document.getElementById("token").value = localStorage.getItem("token");
-
+if(localStorage.getItem("token") == undefined){
+    document.getElementById("back").style.visibility = "hidden";
+}else{
+    document.getElementById("back").style.visibility = "visible";
+}
 function load(){
     localStorage.setItem("username",document.getElementById("username").value);
     localStorage.setItem("token",document.getElementById("token").value);
